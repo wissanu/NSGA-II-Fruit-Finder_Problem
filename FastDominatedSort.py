2a799a68af687bf2def613328533d7552afadccc
Wissanu <thungrut@gmail.com>
2020-08-14 23:18:43 +0700

    Update FastDominatedSort.py

--- FastDominatedSort.py
+++ FastDominatedSort.py
@@ -10,23 +10,14 @@ def fast_non_dominate_sorting(chromosome_list):
     Chromosome.status = 'niche-sort'
     chromosome_list = sorted(chromosome_list)
 
-    # get dominated list ( dominated by whom!!)
-    # !!! The collector need to be sorted before finding dominated solution !!!!
-    for x in chromosome_list:
-        x.dominated_list = [1 if y.fitness_cost > x.fitness_cost and y.fitness_piece > x.fitness_piece else 0 for y in chromosome_list]
-
     # assign pareto rank regraded Front pareto ranking.
-    for index_x, obj in enumerate(chromosome_list):
-        # assign pareto for non-dominated solution
-        if obj.niche_score == 0:
-            obj.pareto_rank = 1
+    for x in chromosome_list:
+        if x.niche_score == 0:
+            x.pareto_rank = 1
         else:
-            # assign pareto for dominated solution
-            nearest_rank = 0
-            for index_y, compare_obj in enumerate(chromosome_list[index_x].dominated_list):
-                if compare_obj == 1:
-                    nearest_rank = chromosome_list[index_y].pareto_rank
-            chromosome_list[index_x].pareto_rank = nearest_rank + 1
+            for y in chromosome_list:
+                if y.fitness_cost > x.fitness_cost and y.fitness_piece > x.fitness_piece:
+                    x.pareto_rank = y.pareto_rank + 1
 
     # sorted list by pareto rank (ASC)
     Chromosome.status = 'pareto-sort'
